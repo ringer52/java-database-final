e9c9a963ba660c3781c1a2a6259af7ce92f31721
Sergey <ringer52@gmail.com>
2026-03-15 17:33:46 +0200

    Add store validation and order placement methods

--- back-end/src/main/java/com/project/code/Controller/StoreController.java
+++ back-end/src/main/java/com/project/code/Controller/StoreController.java
@@ -1,5 +1,18 @@
 package com.project.code.Controller;
 
+import com.project.code.DTO.PlaceOrderRequestDTO;
+import com.project.code.Model.Store;
+import com.project.code.Repo.StoreRepository;
+import com.project.code.Service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -20,4 +33,21 @@ public class StoreController {
         return response;
     }
 
-    @GetMapping("validate/{storeId}
+    @GetMapping("validate/{storeId}")
+    public boolean validateStore(@PathVariable Long storeId) {
+        Optional<Store> store = storeRepository.findById(storeId);
+        return store.isPresent();
+    }
+
+    @PostMapping("/placeOrder")
+    public Map<String, String> placeOrder(@RequestBody PlaceOrderRequestDTO placeOrderRequest) {
+        Map<String, String> response = new HashMap<>();
+        try {
+            orderService.saveOrder(placeOrderRequest);
+            response.put("message", "Order placed successfully");
+        } catch (Exception e) {
+            response.put("Error", e.getMessage());
+        }
+        return response;
+    }
+}
