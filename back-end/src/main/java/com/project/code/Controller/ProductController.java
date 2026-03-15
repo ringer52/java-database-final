1d088c05104a4e416a8b4ea45b6c26d1840b0096
Sergey <ringer52@gmail.com>
2026-03-15 17:35:35 +0200

    Refactor ProductController by removing unused endpoints
    Removed several product-related endpoints from ProductController.

--- back-end/src/main/java/com/project/code/Controller/ProductController.java
+++ back-end/src/main/java/com/project/code/Controller/ProductController.java
@@ -1,5 +1,23 @@
 package com.project.code.Controller;
 
+import com.project.code.Model.Product;
+import com.project.code.Repo.InventoryRepository;
+import com.project.code.Repo.ProductRepository;
+import com.project.code.Service.ServiceClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -55,55 +73,3 @@ public class ProductController {
 
     @GetMapping("/category/{name}/{category}")
     public Map<String, Object> filterbyCategoryProduct(@PathVariable String name,
-                                                        @PathVariable String category) {
-        Map<String, Object> response = new HashMap<>();
-        List<Product> products;
-        if (name.equals("null")) {
-            products = productRepository.findByCategory(category);
-        } else if (category.equals("null")) {
-            products = productRepository.findProductBySubName(name);
-        } else {
-            products = productRepository.findProductBySubNameAndCategory(name, category);
-        }
-        response.put("products", products);
-        return response;
-    }
-
-    @GetMapping
-    public Map<String, Object> listProduct() {
-        Map<String, Object> response = new HashMap<>();
-        List<Product> products = productRepository.findAll();
-        response.put("products", products);
-        return response;
-    }
-
-    @GetMapping("filter/{category}/{storeid}")
-    public Map<String, Object> getProductbyCategoryAndStoreId(@PathVariable String category,
-                                                               @PathVariable Long storeid) {
-        Map<String, Object> response = new HashMap<>();
-        List<Product> products = productRepository.findProductByCategory(category, storeid);
-        response.put("product", products);
-        return response;
-    }
-
-    @DeleteMapping("/{id}")
-    public Map<String, String> deleteProduct(@PathVariable Long id) {
-        Map<String, String> response = new HashMap<>();
-        if (!serviceClass.ValidateProductId(id)) {
-            response.put("message", "Product not present in database");
-            return response;
-        }
-        inventoryRepository.deleteByProductId(id);
-        productRepository.deleteById(id);
-        response.put("message", "Product deleted successfully");
-        return response;
-    }
-
-    @GetMapping("/searchProduct/{name}")
-    public Map<String, Object> searchProduct(@PathVariable String name) {
-        Map<String, Object> response = new HashMap<>();
-        List<Product> products = productRepository.findProductBySubName(name);
-        response.put("products", products);
-        return response;
-    }
-}
