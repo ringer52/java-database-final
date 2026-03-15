900fe2e5d95acaf038305d8113859b350082d397
Sergey <ringer52@gmail.com>
2026-03-15 16:05:04 +0200

    Clean up imports in ProductRepository
    Removed unused imports in ProductRepository.

--- back-end/src/main/java/com/project/code/Repo/ProductRepository.java
+++ back-end/src/main/java/com/project/code/Repo/ProductRepository.java
@@ -1,12 +1,5 @@
 package com.project.code.Repo;
 
-import com.project.code.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
